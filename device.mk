873ed4b4945e1ddb1935b09b02f266231df6702f
7jari <zadigsm@outlook.com>
2026-04-23 17:29:00 +0700

    a05s: build android.hardware.sensors@2.1 from source

--- device.mk
+++ device.mk
@@ -377,7 +377,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal 
+    android.hardware.sensors@2.1-service.multihal
 
 # Service Tracker
 PRODUCT_PACKAGES += \
